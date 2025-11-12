d9a6a32512857e81f61147fb1bb754770f3cf461
vishal092801 <vishalvgg2019@gmail.com>
2025-11-12 19:47:19 +0530

    mistake commit

---
+++ fun.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void count(){
+    int num=1;
+    while (num<=100){
+        cout<<"num ";
+        num++;
+    }
+}
+int main(){
+
+    count();
+    return 0;
+}
