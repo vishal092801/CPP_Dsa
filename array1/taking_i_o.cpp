e42410fa4b92901875f3c13255ca298c4c861150
vishal092801 <vishalvgg2019@gmail.com>
2025-11-12 23:24:21 +0530

    second

---
+++ array1/taking_i_o.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    char ch[5];
+    for(int i=0;i<5;i++){
+        cin>>ch[i];
+    }
+    for(int i=0;i<5;i++){
+        cout<<ch[i]<<endl;
+    }
+
+}
