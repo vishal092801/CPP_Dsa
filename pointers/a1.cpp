8d019bbb11c35c3844f70834f85cd3d424317b47
vishal092801 <vishalvgg2019@gmail.com>
2025-11-18 23:34:19 +0530

    changes

--- pointers/a1.cpp
+++ pointers/a1.cpp
@@ -1,13 +1,17 @@
 #include<iostream>
+using namespace std;
 int main(){
     int a=3;
     int *p=&a;
-    std::cout << *p; // with the using of *p we will get the value of a, or what value is
+    std::cout << *p<<endl; // with the using of *p we will get the value of a, or what value is
                     // store in the address of p pointer.
 
 
-    std::cout << p; // this is shows the address of a variable
-    
+    std::cout << p<<endl; // this is shows the address of a variable
+
+    int **pp=&p;
+    cout<<pp<<endl;
+    cout<<*pp<<endl;
 
      
 
