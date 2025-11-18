3f8aac7c64509bdbf8a21b47a39c49b80efcdb94
vishal092801 <vishalvgg2019@gmail.com>
2025-11-19 00:30:45 +0530

    cngs

--- pointers/callbyvalue.cpp
+++ pointers/callbyvalue.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 using namespace std;
 void change(int s){
-    int a=20;
-    cout<<a<<endl;
+    int b;
+    cout<<"enter the value of b"<<endl;
+    cin>>b;
+    s=b;
+    cout<<s<<endl;
 }
 int main(){
     int a=90;
@@ -10,5 +13,5 @@ int main(){
     cout<<a;
     return 0;
 }
-/*In the case of call by value we make a copy of var. in a function 
+/*In the case of call by value we make a copy of var. in a functio
 here no change will be occurs in the main function */
