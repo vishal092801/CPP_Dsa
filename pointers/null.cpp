a2b07c11f82672e8a5a9800ccb352ce0688230be
vishal092801 <vishalvgg2019@gmail.com>
2025-11-18 23:59:39 +0530

    first

---
+++ pointers/null.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int *ptr=NULL;
+    cout<<ptr;
+}
+
+/*normally pointers shows the address of any variable, and when we declar a variable 
+and dont assign any address of any var. then it shows a garbage value*/
